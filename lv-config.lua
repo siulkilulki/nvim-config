cf63525e7980b3f4c7f85ca2ec75eb35eb3bd175
Dawid Jurkiewicz <dawid.jurkiewicz@applica.ai>
2021-08-03 17:20:32 +0200

    Add neogit, vim-sneak and basic keybindings

--- lv-config.lua
+++ lv-config.lua
@@ -8,22 +8,20 @@ lvim.colorscheme = "spacegray"
 -- keymappings
 lvim.leader = "space"
 -- overwrite the key-mappings provided by LunarVim for any mode, or leave it empty to keep them
--- lvim.keys.normal_mode = {
---   Page down/up
---   {'[d', '<PageUp>'},
---   {']d', '<PageDown>'},
---
---   Navigate buffers
---   {'<Tab>', ':bnext<CR>'},
---   {'<S-Tab>', ':bprevious<CR>'},
--- }
+lvim.keys.normal_mode = {
+  {'<TAB>', '<C-w>w'},
+  {'<C-l>', ':BufferNext<CR>'},
+  {'<C-h>', ':BufferPrevious<CR>'},
+  {'<C-x>', ':bd<CR>'},
+}
 -- if you just want to augment the existing ones then use the utility function
--- require("utils").add_keymap_insert_mode({ silent = true }, {
--- { "<C-s>", ":w<cr>" },
--- { "<C-c>", "<ESC>" },
--- })
--- you can also use the native vim way directly
--- vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
+require("utils").add_keymap_normal_mode({ silent = true }, {
+  {'<S-l>', ':BufferMoveNext<CR>'},
+  {'<S-h>', ':BufferMovePrevious<CR>'},
+})
+
+-- Swap ':' with ';'
+vim.api.nvim_set_keymap('', ';', ':', {noremap = true})
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -37,6 +35,9 @@ lvim.builtin.treesitter.ensure_installed = {}
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
+-- Python
+lvim.lang.python.formatter.exe = "black"
+
 -- generic LSP settings
 -- you can set a custom on_attach function that will be used for all the language servers
 -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
@@ -49,13 +50,17 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- end
 
 -- Additional Plugins
--- lvim.plugins = {
---     {"folke/tokyonight.nvim"}, {
---         "ray-x/lsp_signature.nvim",
---         config = function() require"lsp_signature".on_attach() end,
---         event = "InsertEnter"
---     }
--- }
+lvim.plugins = {
+    {
+      "justinmk/vim-sneak",
+      event = "BufRead"
+    },
+    {
+      'TimUntersberger/neogit',
+      requires = 'nvim-lua/plenary.nvim',
+      event = "BufRead"
+    }
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
@@ -63,3 +68,14 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- }
 
 -- Additional Leader bindings for WhichKey
+lvim.builtin.which_key.mappings["g"].g = { "<cmd>Neogit<CR>", "Neogit" }
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>lua require'telescope'.extensions.project.project{}<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+-- }
